fa7ac6f7efeaa638c81c66bb147026be3823a137
unknown <suravi.panigrahi98@gmail.com>
2018-12-24 22:07:44 +0530

    Init commit

---
+++ playground/mongodbconnect.js
@@ -0,0 +1,35 @@
+// var MongoClient=require('mongodb').MongoClient;
+// for destructuring:can pull any property from obj 'mongodb'
+const {MongoClient,ObjectID}=require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+	if(err){
+		return console.log('Unable to connect to MongoDB server');
+	}
+	console.log('Connected to MongoDB server');
+
+	// db.collection('Todos').insertOne({
+	// 	text:'buy books',
+	// 	completed:false
+	// },(err,result)=>{
+	// 	if(err){
+	// 		return console.log('Unable to insert todo',err);
+	// 	}
+	// 	console.log(JSON.stringify(result.ops,undefined,2));
+	// });
+
+
+	db.collection('Users').insertOne({
+		name:'Ram',
+		age:19,
+		location:'Delhi'
+	},(err,result)=>{
+		if(err){
+			return console.log('Unable to insert user',err);
+		}
+		console.log(result.ops[0]._id.getTimestamp()); 
+	});
+	db.close();
+});
+
+
